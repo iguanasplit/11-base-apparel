796408a5fe2ab2fff6cf92c3f6329f5b6ddc08a9
iguanasplit <111544069+iguanasplit@users.noreply.github.com>
2022-10-07 19:37:51 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 
+
+document.addEventListener('invalid', (function () {
+    return function (e) {
+        //prevent the browser from showing default error bubble / hint
+        e.preventDefault();
+        // optionally fire off some custom validation handler
+        // myValidation();
+    };
+})(), true);
+
+const button = document.getElementById('submit');
+//   const email = document.getElementById('email');
+
+button.addEventListener('click', function () {
+
+    const email = document.getElementById('email').checkValidity();
+    console.log(email);
+
+
+    let element = document.querySelector('.forma');
+    let errorIcon = document.querySelector('.error-icon');
+    let errorSubmission = document.querySelector(".errorsubmission");
+
+    if (email) {
+        element.classList.remove("invalid");
+        // alert("Hey there!");
+        errorIcon.style.display = "none";
+        errorSubmission.style.display = "none";
+    } else {
+
+        element.classList.add("invalid");
+        // alert("Hey there!");
+        errorIcon.style.display = "inline";
+        errorSubmission.style.display = "inline";
+    }
+
+
+}
+
+
+);
